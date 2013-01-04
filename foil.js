eae598e9d880432806a42c79bebadd3ce49b488d
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-01-04 19:09:39 +0100

    Just foiling around

---
+++ foil.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+//
+// @name           Foil
+// @description    Does weird stuff on StackExchange sites
+// @homepage       http://github.com/oliversalzburg/foil/
+// @namespace      http://oliversalzburg.github.com/
+// @author         Oliver Salzburg, oliversalzburg (http://github.com/oliversalzburg/)
+// @license        MIT License (http://opensource.org/licenses/mit-license.php)
+//
+// @include        http://superuser.com/*
+//
+// @require        https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+//
+// @version        0.1
+// @history        0.1 Initial release
+//
+// ==/UserScript==
+
+
+$(function(){
+  window.alert("Just foiling around.");
+});
